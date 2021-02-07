c69febc240592fb6437a34987690f5d2c9c19bbd
acalabro <acalabro@X1>
2021-02-07 19:43:51 +0100

    Fixed dependency with vulnerability

--- src/main/java/it/cnr/isti/labsedc/glimpse/example/MyGlimpseSmartCampusProbe.java
+++ src/main/java/it/cnr/isti/labsedc/glimpse/example/MyGlimpseSmartCampusProbe.java
@@ -2,24 +2,23 @@
   * GLIMPSE: A generic and flexible monitoring infrastructure.
 
   * For further information: http://labsewiki.isti.cnr.it/labse/tools/glimpse/public/main
-  * 
+  *
   * Copyright (C) 2015  Software Engineering Laboratory - ISTI CNR - Pisa - Italy
-  * 
+  *
   * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
   * the Free Software Foundation, either version 3 of the License, or
   * (at your option) any later version.
-  * 
+  *
   * This program is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   * GNU General Public License for more details.
-  * 
+  *
   * You should have received a copy of the GNU General Public License
   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  * 
+  *
 */
-
 package it.cnr.isti.labsedc.glimpse.example;
 
 import java.lang.management.ManagementFactory;
@@ -48,20 +47,19 @@ public class MyGlimpseSmartCampusProbe extends GlimpseAbstractProbe {
 	 * @version 3.3.2
 	 *
 	 */
-	
+
 	public static int sendingInterval = 10000;
-	
 	public static String parameterName;
 	public static float parameterValue;
 	public static String roomID;
 	public static String sensorName;
 	public static Random rand = new Random();
-	
+
 	public MyGlimpseSmartCampusProbe(Properties settings) {
 		super(settings);
 	}
 
-	public static void main(String[] args) throws UnknownHostException {
+	public static void main(String[] args) throws UnknownHostException, InterruptedException {
 
 		//creating a probe
 		DebugMessages.line();
@@ -69,48 +67,51 @@ public class MyGlimpseSmartCampusProbe extends GlimpseAbstractProbe {
 				Manager.createProbeSettingsPropertiesObject("org.apache.activemq.jndi.ActiveMQInitialContextFactory",
 								//"ssl://146.48.77.37:61617","system", "manager","TopicCF",
 							  "tcp://glimpse-dev.isti.cnr.it:61616","system", "manager","TopicCF",
-								"jms.probeTopic", 
-								false, 
+								"jms.probeTopic",
+								false,
 								"probeName",
-								"it.cnr.isti.labsedc.glimpse,java.lang,javax.security,java.util", 								 
+								"it.cnr.isti.labsedc.glimpse,java.lang,javax.security,java.util",
 								"/home/acalabro/workspace/GlimpseSmartCampusProbe/probe.ks",
 								"/home/acalabro/workspace/GlimpseSmartCampusProbe/probe.ts",
 								"n1hehe", "n1hehe"));
-		
+
 		//sending events
 		try {
-				aGenericProbe.generateAndSendExample_GlimpseBaseEvents_SmartBuildingPayload();
-				aGenericProbe.generateAndSendExample_GlimpseBaseEvents_FaceRecognitionPayload();
-				aGenericProbe.generateAndSendExample_GlimpseBaseEvents_EventMachineInformationPayload();
-		} catch (IndexOutOfBoundsException e) {}		
+//				aGenericProbe.generateAndSendExample_GlimpseBaseEvents_SmartBuildingPayload();
+//				aGenericProbe.generateAndSendExample_GlimpseBaseEvents_FaceRecognitionPayload();
+			int i = 0;
+			while (i < 1000000) {
+			aGenericProbe.generateAndSendExample_GlimpseBaseEvents_EventMachineInformationPayload();
+			i++;
+			}
+		} catch (IndexOutOfBoundsException e) {}
 	}
-	
+
 	private void generateAndSendExample_GlimpseBaseEvents_EventMachineInformationPayload() {
 		DebugMessages.ok();
-		DebugMessages.print(System.currentTimeMillis(), 
+		DebugMessages.print(System.currentTimeMillis(),
 				MyGlimpseSmartCampusProbe.class.getSimpleName(),
 				"Creating GlimpseBaseMachineInformation message");
 		GlimpseBaseEventMachineInformation<String> message;
 		DebugMessages.ok();
 		DebugMessages.line();
-		
+
 		message = new GlimpseBaseEventMachineInformation<String>(
-				System.getProperty("os.name"), 
-				this.getClass().getCanonicalName(), 
-				System.currentTimeMillis(), 
+				System.getProperty("os.name"),
+				this.getClass().getCanonicalName(),
+				System.currentTimeMillis(),
 				"EventMachineInformationEvent",
 				false,
 				"noExtraFields",
 				"Available cpu: " + String.valueOf(Runtime.getRuntime().availableProcessors()),
-				(new Double(getProcessCpuLoad()).longValue()), 
+				(new Double(getProcessCpuLoad()).longValue()),
 				Runtime.getRuntime().freeMemory()/1024,
 				Runtime.getRuntime().totalMemory()/1024,
 				0l,
 				0l,
 				0l,
 				0l);
-		
-				
+
 			try {
 				this.sendEventMessage(message, false);
 				DebugMessages.println(System.currentTimeMillis(), MyGlimpseSmartCampusProbe.class.getSimpleName(),
@@ -123,53 +124,26 @@ public class MyGlimpseSmartCampusProbe extends GlimpseAbstractProbe {
 	}
 
 	public static double getProcessCpuLoad() {
-		
-		OperatingSystemMXBean sysInfo = ManagementFactory.getOperatingSystemMXBean(); 
+
+		OperatingSystemMXBean sysInfo = ManagementFactory.getOperatingSystemMXBean();
 		return sysInfo.getSystemLoadAverage();
-//	    MBeanServer mbs    = ManagementFactory.getPlatformMBeanServer();
-//	    ObjectName name;
-//	    Double value = 0d;
-//		try {
-//			name = ObjectName.getInstance("java.lang:type=OperatingSystem");
-//		    AttributeList list = mbs.getAttributes(name, new String[]{ "ProcessCpuLoad" });
-//		    if (list.isEmpty())     return Double.NaN;
-//
-//		    Attribute att = (Attribute)list.get(0);
-//		    value  = (Double)att.getValue();
-//
-//		    // usually takes a couple of seconds before we get real values
-//		    if (value == -1.0)      return Double.NaN;
-//		    // returns a percentage value with 1 decimal point precision
-//		    
-//		} catch (MalformedObjectNameException | NullPointerException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (InstanceNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (ReflectionException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//		return ((int)(value * 1000) / 10.0);
 	}
 
 	@Override
-	public void sendMessage(GlimpseBaseEvent<?> event, boolean debug) {		
+	public void sendMessage(GlimpseBaseEvent<?> event, boolean debug) {
 	}
-	
+
 	private void generateAndSendExample_GlimpseBaseEvents_SmartBuildingPayload() {
-		
+
 		DebugMessages.ok();
 		DebugMessages.print(System.currentTimeMillis(), MyGlimpseSmartCampusProbe.class.getSimpleName(),"Creating GlimpseBaseEventSB message");
 		GlimpseBaseEventSB<Float> message;
 		DebugMessages.ok();
 		DebugMessages.line();
-		
+
 		message = new GlimpseBaseEventSB<Float>(64f, "HumiditySensorName",
 						System.currentTimeMillis(),	"Humidity",	false, "i-30", SensorType.HUMIDITY);
-				
+
 			try {
 				this.sendEventMessage(message, false);
 				DebugMessages.println(System.currentTimeMillis(),
@@ -187,17 +161,17 @@ public class MyGlimpseSmartCampusProbe extends GlimpseAbstractProbe {
 				e.printStackTrace();
 			}
 		}
-	
+
 	private void generateAndSendExample_GlimpseBaseEvents_FaceRecognitionPayload() {
 		DebugMessages.print(System.currentTimeMillis(), MyGlimpseSmartCampusProbe.class.getSimpleName(),"Creating GlimpseBaseEventFaceRecognition message");
 		GlimpseBaseEventFaceRecognition<Boolean> message;
 		DebugMessages.ok();
 		DebugMessages.line();
-		
+
 		message = new GlimpseBaseEventFaceRecognition<Boolean>(
 				false, "CameraName", System.currentTimeMillis(),
 				"StringParameterAvailable", false, "i-30", "Unknown", "IDScreenshot");
-				
+
 			try {
 				this.sendEventMessage(message, false);
 				DebugMessages.println(System.currentTimeMillis(),
--- src/main/java/it/cnr/isti/labsedc/glimpse/probe/GlimpseAbstractProbe.java
+++ src/main/java/it/cnr/isti/labsedc/glimpse/probe/GlimpseAbstractProbe.java
@@ -1,22 +1,22 @@
  /*
   * GLIMPSE: A generic and flexible monitoring infrastructure.
   * For further information: http://labsewiki.isti.cnr.it/labse/tools/glimpse/public/main
-  * 
+  *
   * Copyright (C) 2011  Software Engineering Laboratory - ISTI CNR - Pisa - Italy
-  * 
+  *
   * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
   * the Free Software Foundation, either version 3 of the License, or
   * (at your option) any later version.
-  * 
+  *
   * This program is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   * GNU General Public License for more details.
-  * 
+  *
   * You should have received a copy of the GNU General Public License
   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  * 
+  *
 */
 
 package it.cnr.isti.labsedc.glimpse.probe;
@@ -49,13 +49,13 @@ import it.cnr.isti.labsedc.glimpse.utils.Manager;
  * This class represent a generic implementation of the interface {@link GlimpseProbe}.<br />
  * It provides the abstract method: {@link #sendMessage(GlimpseBaseEvent, boolean)}<br />
  * that can be extended if needed.<br />
- * 
+ *
  * @author Antonello Calabr&ograve;
  * @version 3.2
  *
  */
 public abstract class GlimpseAbstractProbe implements GlimpseProbe {
-	
+
 	protected static InitialContext initContext;
 	protected static TopicSession publishSession;
 	protected static TopicPublisher tPub;
@@ -64,24 +64,24 @@ public abstract class GlimpseAbstractProbe implements GlimpseProbe {
 	protected static int MESSAGEID = 0;
 	protected boolean retry = true;
 	protected int seconds = 5;
-	
+
 	/**
 	 * This constructor allow to create a GlimpseAbstractProbe object<br />
 	 * providing the {@link Properties} settings object
 	 * @param settings can be generated automatically
 	 * using {@link Manager#createConsumerSettingsPropertiesObject(String, String, String, String, String, String, boolean, String)}.
-	 * @throws InterruptedException 
-	 * 
-	 */	
+	 * @throws InterruptedException
+	 *
+	 */
 	public GlimpseAbstractProbe(Properties settings) {
-		
+
 		while (retry) {
 			try {
 				initContext = this.initConnection(settings, true);
 				if (settings.getProperty("java.naming.provider.url").startsWith("ssl")) {
-					this.createSSLConnection(initContext,settings.getProperty("topic.probeTopic").replaceFirst("jms.", ""), settings, true);	
+					this.createSSLConnection(initContext,settings.getProperty("topic.probeTopic").replaceFirst("jms.", ""), settings, true);
 				}
-				else 
+				else
 					this.createConnection(initContext, settings, true);
 				retry = false;
 			} catch (NamingException | JMSException e) {
@@ -98,14 +98,14 @@ public abstract class GlimpseAbstractProbe implements GlimpseProbe {
 				seconds +=5;
 				DebugMessages.println(System.currentTimeMillis(), this.getClass().getSimpleName(), "Retrying .......... ");
 				}
-				
+
 			}
 		}
 	}
-	
+
 	/**
 	 * This method setup a {@link TopicConnection} object.
-	 * 
+	 *
 	 * @param initConn the InitialContext object generated using the method {@link #initConnection(Properties, boolean)}.
 	 * @param settings can be generated automatically using {@link Manager#createProbeSettingsPropertiesObject(String, String, String, String, String, String, boolean, String, String)}
 	 * @param probeChannel
@@ -115,9 +115,9 @@ public abstract class GlimpseAbstractProbe implements GlimpseProbe {
 	 * @throws NamingException
 	 * @throws JMSException
 	 */
-	
-	
-	
+
+
+
 	 protected TopicPublisher createSSLConnection(InitialContext initConn, String probeTopic, Properties settings, boolean debug) throws NamingException, JMSException
 		{
 			if (debug) {
@@ -125,21 +125,21 @@ public abstract class GlimpseAbstractProbe implements GlimpseProbe {
 						"Creating SSL ConnectionFactory with settings ");
 				try {
 				ActiveMQSslConnectionFactory connFact;
-			
-				
-				System.setProperty("javax.net.ssl.keyStore", settings.getProperty("keyStore")); 
-			    System.setProperty("javax.net.ssl.keyStorePassword", settings.getProperty("keyStorePassword")); 
-			    System.setProperty("javax.net.debug", "handshake"); 
-		
+
+
+				System.setProperty("javax.net.ssl.keyStore", settings.getProperty("keyStore"));
+			    System.setProperty("javax.net.ssl.keyStorePassword", settings.getProperty("keyStorePassword"));
+			    System.setProperty("javax.net.debug", "handshake");
+
 			    connFact = new ActiveMQSslConnectionFactory(settings.getProperty("java.naming.provider.url"));
-			    
+
 				connFact.setTrustStore(settings.getProperty("trustStore"));
-			    connFact.setTrustStorePassword(settings.getProperty("trustStorePassword")); 
+			    connFact.setTrustStorePassword(settings.getProperty("trustStorePassword"));
 				connFact.setTrustAllPackages(true);
 				connFact.setTrustedPackages(new ArrayList<String>(Arrays.asList(settings.getProperty("activemq.trustable.serializable.class.list").split(","))));
 
-			
-				DebugMessages.ok();  
+
+				DebugMessages.ok();
 				DebugMessages.print(System.currentTimeMillis(), this.getClass().getSimpleName(),
 							"Creating TopicConnection ");
 				connection = connFact.createTopicConnection();
@@ -166,9 +166,9 @@ public abstract class GlimpseAbstractProbe implements GlimpseProbe {
 			}
 				}
 			return null;
-			
+
 		}
-	 
+
 	 protected TopicPublisher createConnection(InitialContext initConn, Properties settings, boolean debug) throws NamingException, JMSException
 		{
 			if (debug) {
@@ -176,10 +176,10 @@ public abstract class GlimpseAbstractProbe implements GlimpseProbe {
 						"Creating ConnectionFactory with settings ");
 				try {
 				ActiveMQConnectionFactory connFact;
-				
+
 			    connFact = new ActiveMQConnectionFactory(settings.getProperty("java.naming.provider.url"));
-	
-				DebugMessages.ok();  
+
+				DebugMessages.ok();
 				DebugMessages.print(System.currentTimeMillis(), this.getClass().getSimpleName(),
 							"Creating TopicConnection ");
 				connection = connFact.createTopicConnection();
@@ -206,14 +206,14 @@ public abstract class GlimpseAbstractProbe implements GlimpseProbe {
 			}
 				}
 			return null;
-			
+
 		}
-    
-    
-	
+
+
+
 	/**
 	 * This method setup the connection parameters using the {@link Properties} object {@link #settings}
-	 * 
+	 *
 	 * @param settings the parameter to setup the connection to the Enterprise Service Bus<br /> to send messages
 	 * @param debug
 	 * @return it provides an {@link InitialContext} object that will be used<br />during the Consumer <-> Monitoring interaction.
@@ -225,18 +225,18 @@ public abstract class GlimpseAbstractProbe implements GlimpseProbe {
 					"Creating InitialContext with settings ");
 			InitialContext initConn = new InitialContext(settings);
 			if (debug) {
-				DebugMessages.ok(); 
+				DebugMessages.ok();
 				DebugMessages.line(); }
 			return initConn;
-		}	
-		
-		
+		}
+
+
 	public abstract void sendMessage(GlimpseBaseEvent<?> event, boolean debug);
-	
+
 	/**
 	 * This method send a {@link GlimpseBaseEvent} message on the ESB<br />
 	 * specifically on the channel specified in the {@link #settings} object.
-	 * 
+	 *
 	 * @param event the event to send
 	 * @param debug
 	 * @throws JMSException
@@ -247,18 +247,18 @@ public abstract class GlimpseAbstractProbe implements GlimpseProbe {
 		if (debug) {
 			DebugMessages.print(System.currentTimeMillis(), this.getClass().getSimpleName(),
 					"Creating Message "); }
-		try 
+		try
 		{
-			ObjectMessage messageToSend = publishSession.createObjectMessage();	
+			ObjectMessage messageToSend = publishSession.createObjectMessage();
 			messageToSend.setJMSMessageID(String.valueOf(MESSAGEID++));
-			messageToSend.setObject(event);		
+			messageToSend.setObject(event);
 		if (debug) {
 			DebugMessages.ok();
 			DebugMessages.print(System.currentTimeMillis(), this.getClass().getSimpleName(),
 					"Publishing message  "); }
-		tPub.publish(messageToSend);	
+		tPub.publish(messageToSend);
 		if (debug) {
-			DebugMessages.ok(); 
+			DebugMessages.ok();
 			DebugMessages.line(); }
 		} catch (JMSException e) {
 			e.printStackTrace();
